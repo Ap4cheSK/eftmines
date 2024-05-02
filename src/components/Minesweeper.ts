d560b06c27a4a2c10cc1b43cb47b62716751ad46
Ap4cheSK <apache@atlas.sk>
2024-05-02 14:15:04 +0200

    Fixed mines indicators, added comments

--- src/components/Minesweeper.ts
+++ src/components/Minesweeper.ts
@@ -1,3 +1,9 @@
+/**
+ * Minesweeper game logic.
+ * @param {number} sizeX - Width of gameboard
+ * @param {number} sizeY - Height of gameboard
+ * @param {number} mines - Number of mines in gameboard
+ */
 class Minesweeper {
 	sizeX: number;
 	sizeY: number;
@@ -10,17 +16,24 @@ class Minesweeper {
 		this.mines = mines;
 
 		for(let row = 0; row < this.sizeY; row++) {
+			this.board[row] = [];
 			for(let column = 0; column < this.sizeX; column++) {
-				this.board[row][column] = " ";
+				this.board[row][column] = "_";
 			}
 		}
 	}
 
+	/**
+	 * Minesweeper internal method
+	 */
 	generateRandomPos(max: number) {
 		// Generates from 0 (inclusive) to max (exclusive)
 		return Math.floor(Math.random() * max);
 	}
 
+	/**
+	 * Minesweeper internal method
+	 */
 	generateMines() {
 		let minesGenerated = 0;
 
@@ -36,7 +49,14 @@ class Minesweeper {
 		}
 	}
 
+	/**
+	 * Minesweeper internal method
+	 */
 	generateIndicator(posY: number, posX: number) {
+		if(this.board[posY][posX] === "X") {
+			return "X";
+		}
+
 		let startY = posY - 1;
 		let startX = posX - 1;
 		let endY = posY + 1;
@@ -56,27 +76,53 @@ class Minesweeper {
 
 		let minesNearby = 0;
 
-		for(let row = startY; row < endY; row++) {
-			for(let column = startX; column < endX; column++) {
+		for(let row = startY; row <= endY; row++) {
+			for(let column = startX; column <= endX; column++) {
 				if(this.board[row][column] === "X")
 					minesNearby++;
 			}
 		}
 
 		if(minesNearby === 0)
-			return " ";
+			return "_";
 
 		return minesNearby.toString();
 	}
 
+	/**
+	 * Minesweeper internal method
+	 */
 	generateMineIndicators() {
 		for(let row = 0; row < this.sizeY; row++) {
 			for(let column = 0; column < this.sizeX; column++) {
-				if(this.board[row][column] === "X")
-					continue;
-
 				this.board[row][column] = this.generateIndicator(row, column);
 			}
 		}
 	}
+
+	/**
+	 * Method to initialize board. Generate mines, generate mine indicators (numbers)
+	 */
+	initBoard() {
+		this.generateMines();
+		this.generateMineIndicators();
+	}
+
+	/**
+	 * [DEBUG] Prints game board, meant to debug behaviours of the game logic
+	 */
+	printBoard() {
+		console.log("Game Board:")
+		for(let row = 0; row < this.sizeY; row++) {
+			let rowString = "";
+
+			for(let column = 0; column < this.sizeX; column++) {
+				rowString = rowString + this.board[row][column] + " ";
+			}
+
+			console.log(rowString);
+		}
+	}
 }
+
+export default Minesweeper;
