78bb896d7b922593ad113130ca5ecbcdfc81d05b
Ap4cheSK <apache@atlas.sk>
2024-05-02 16:26:35 +0200

    added buttons to play game by difficulties

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -11,7 +11,9 @@ function Homepage() {
 			<AppHeader/>
 			<main className="main-menu-container">
 				<nav className="main-menu">
-					<Link to={"/game"} className="LinkButton" aria-label="Play game">Play</Link>
+					<Link to={"/game/0"} className="LinkButton" aria-label="Play game">Easy (8x8)</Link>
+					<Link to={"/game/1"} className="LinkButton" aria-label="Play game">Medium (8x12)</Link>
+					<Link to={"/game/2"} className="LinkButton" aria-label="Play game">Hard (12x12)</Link>
 					<button onClick={redirect} className="LinkButton" aria-label="Back to Tarkovbot.eu">TarkovBOT.eu</button>
 				</nav>
 				<p className="game-version">Version 1.0.0</p>
