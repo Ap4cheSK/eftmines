dc7235b5ff9b24f0f24fa1bc481b7edab7960320
Ap4cheSK <apache@atlas.sk>
2024-05-02 23:22:44 +0200

    click handling

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -8,17 +8,27 @@ interface gameBoardInterface {
 	sizeX: number;
 	sizeY: number;
 	showMines: boolean;
+	moveX: React.Dispatch<React.SetStateAction<number>>
+	moveY: React.Dispatch<React.SetStateAction<number>>
+	moveType: React.Dispatch<React.SetStateAction<number>>
+	moveTimestamp: React.Dispatch<React.SetStateAction<number>>
 }
 
-interface SquareInterface {
+interface CellInterface {
 	isMine: boolean;
 	flag: boolean;
 	show: boolean;
 	minesNear: number;
+	rowIdx: number;
+	cellIdx: number;
+	moveX: React.Dispatch<React.SetStateAction<number>>
+	moveY: React.Dispatch<React.SetStateAction<number>>
+	moveType: React.Dispatch<React.SetStateAction<number>>
+	moveTimestamp: React.Dispatch<React.SetStateAction<number>>
 }
 
 function StatusBar() {
-	function redirect() {
+	function newGame() {
 		window.location.reload();
 	}
 
@@ -26,41 +36,50 @@ function StatusBar() {
 		<section className="status-bar">
 			<div className="status-item">
 				<h3>Mines</h3>
-				<p>0</p>
+				<p>not functional</p>
 			</div>
 
-			<button onClick={redirect} className="new-game" aria-label="New Game">New Game</button>
+			<button onClick={newGame} className="new-game" aria-label="New Game">New Game</button>
 
 			<div className="status-item">
 				<h3>Time</h3>
-				<p>0</p>
+				<p>not functional</p>
 			</div>
 		</section>
 	);
 }
 
-function Cell(cell: SquareInterface) {
+function Cell(cell: CellInterface) {
+	function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+		event.preventDefault();
+
+		cell.moveX(cell.cellIdx);
+		cell.moveY(cell.rowIdx);
+		cell.moveType(event.button);
+		cell.moveTimestamp(Date.now());
+	}
+
 	if(!cell.show) {
 		return (
-			<div className="game-cell"></div>
+			<div onClick={handleClick} onContextMenu={handleClick} className="game-cell"></div>
 		);
 	}
 
 	if(cell.show) {
 		if(cell.isMine) {
 			return (
-				<div className="game-cell mine"><i className="fa-solid fa-bomb"></i></div>
+				<div onClick={handleClick} onContextMenu={handleClick} className="game-cell mine"><i className="fa-solid fa-bomb"></i></div>
 			);
 		}
 
 		if(cell.minesNear === 0) {
 			return (
-				<div className="game-cell number"></div>
+				<div onClick={handleClick} onContextMenu={handleClick} className="game-cell number"></div>
 			);
 		}
 
 		return (
-			<div className="game-cell number">{cell.minesNear}</div>
+			<div onClick={handleClick} onContextMenu={handleClick} className="game-cell number">{cell.minesNear}</div>
 		);
 	}
 
@@ -72,12 +91,31 @@ function Cell(cell: SquareInterface) {
 }
 
 function GameBoard(game: gameBoardInterface) {
+	const [moveX, setMoveX] = useState(-1);
+	const [moveY, setMoveY] = useState(-1);
+	const [moveType, setMoveType] = useState(-1);
+	const [moveTimestamp, setMoveTimestamp] = useState(0);
+
+	useEffect(() => {
+		game.moveX(moveX);
+		game.moveY(moveY);
+		game.moveType(moveType);
+		game.moveTimestamp(moveTimestamp);
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [moveX, moveY, moveType, moveTimestamp]);
+
 	return (
 		<section className="game-board">
 			{game.board.map((row, rowIdx) => (
 				<div className="game-row" key={rowIdx}>
 					{row.map((cell, cellIdx) => (
 						<Cell
+							moveTimestamp={setMoveTimestamp}
+							moveX={setMoveX}
+							moveY={setMoveY}
+							moveType={setMoveType}
+							rowIdx={rowIdx}
+							cellIdx={cellIdx}
 							key={cellIdx}
 							isMine={cell === "X" ? true : false}
 							flag={cell === "F" ? true : false}
@@ -97,11 +135,15 @@ function GamePage() {
 	const [sizeX, setSizeX] = useState(0);
 	const [sizeY, setSizeY] = useState(0);
 	const [showMines, setShowMines] = useState(false);
+	const [moveX, setMoveX] = useState(-1);
+	const [moveY, setMoveY] = useState(-1);
+	const [moveType, setMoveType] = useState(-1);
+	const [moveTimestamp, setMoveTimestamp] = useState(-1);
 
 	useEffect(() => {
 		const gameInitX = [8, 8, 12];
 		const gameInitY = [8, 12, 12];
-		const gameInitMines = [16, 24, 32];
+		const gameInitMines = [12, 24, 32];
 		let diff = 0;
 
 		if(difficulty)
@@ -117,12 +159,31 @@ function GamePage() {
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	useEffect(() => {
+		if(moveX !== -1 && moveY !== -1 && moveType !== -1 && moveTimestamp !== -1) {
+			console.log(`X:${moveX} Y:${moveY} T:${moveType}`);
+
+			setMoveX(-1);
+			setMoveY(-1);
+			setMoveType(-1);
+		}
+	}, [moveX, moveY, moveType, moveTimestamp]);
+
 	return (
 		<>
 			<AppHeader homeBtn={true}/>
 			<main className="game-container">
 				<StatusBar/>
-				<GameBoard board={boardData} sizeX={sizeX} sizeY={sizeY} showMines={showMines}/>
+				<GameBoard
+					moveTimestamp={setMoveTimestamp}
+					moveX={setMoveX}
+					moveY={setMoveY}
+					moveType={setMoveType}
+					board={boardData}
+					sizeX={sizeX}
+					sizeY={sizeY} 
+					showMines={showMines}
+				/>
 			</main>
 			<div></div>
 		</>
