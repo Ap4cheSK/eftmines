d8b81362e6e9e64b5d3841167c82e067cfe33aa6
Ap4cheSK <apache@atlas.sk>
2024-05-09 22:43:02 +0200

    status bar dynamic data implementation

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -6,6 +6,7 @@ import Minesweeper from "./Minesweeper";
 import AppHeader from "./AppHeader";
 // Game Components
 import GameBoard from "./GameComponents/GameBoard";
+import StatusBar from "./GameComponents/StatusBar";
 
 type GameArray = {
 	type: string;
@@ -13,34 +14,13 @@ type GameArray = {
 	flag: boolean;
 }[][];
 
-function StatusBar() {
-	function newGame() {
-		window.location.reload();
-	}
-
-	return (
-		<section className="status-bar">
-			<div className="status-item">
-				<h3>Mines</h3>
-				{/* <p>not functional</p> */}
-			</div>
-
-			<button onClick={newGame} className="new-game" aria-label="New Game">New Game</button>
-
-			<div className="status-item">
-				<h3>Time</h3>
-				{/* <p>not functional</p> */}
-			</div>
-		</section>
-	);
-}
-
 function GamePage() {
 	const { difficulty } = useParams();
 	const [boardData, setBoardData] = useState<GameArray>([]);
 	const [sizeX, setSizeX] = useState(0);
 	const [sizeY, setSizeY] = useState(0);
-	const [showMines, setShowMines] = useState(false);
+	const [gameStatus, setGameStatus] = useState("playing");
+	const [minesCount, setMinesCount] = useState(0);
 	const [moveX, setMoveX] = useState(-1);
 	const [moveY, setMoveY] = useState(-1);
 	const [moveType, setMoveType] = useState(-1);
@@ -48,33 +28,38 @@ function GamePage() {
 
 	const gameInitX = [8, 8, 12];
 	const gameInitY = [8, 12, 12];
-	const gameInitMines = [12, 24, 32];
+	const gameInitMines = [12, 20, 40];
 
 	useEffect(() => {		
 		let diff = 0;
 		if(difficulty)
 			diff = parseInt(difficulty);
 
-		const game = new Minesweeper(gameInitX[diff], gameInitY[diff], gameInitMines[diff]);
+		const game = new Minesweeper(gameInitX[diff], gameInitY[diff], gameInitMines[diff], 0);
 
 		game.initBoard();
 
 		setBoardData(game.board);
 		setSizeX(game.sizeX);
 		setSizeY(game.sizeY);
-		setShowMines(false);
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		setGameStatus("playing");
+		setMinesCount(game.mines - game.flags);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	// Player's move handling
 	useEffect(() => {
-		if(moveX !== -1 && moveY !== -1 && moveType !== -1 && moveTimestamp !== -1) {
-			console.log(`X:${moveX} Y:${moveY} T:${moveType}`);
+		if(gameStatus !== "playing")
+			return;
 
+		if(moveX !== -1 && moveY !== -1 && moveType !== -1 && moveTimestamp !== -1) {
 			let diff = 0;
 			if(difficulty)
 				diff = parseInt(difficulty);
 
-			const game = new Minesweeper(gameInitX[diff], gameInitY[diff], gameInitMines[diff]);
+			const flagCount = gameInitMines[diff] - minesCount;
+
+			const game = new Minesweeper(gameInitX[diff], gameInitY[diff], gameInitMines[diff], flagCount);
 			
 			game.loadBoard(boardData);
 
@@ -87,15 +72,17 @@ function GamePage() {
 			}
 
 			setBoardData(game.board);
+			setGameStatus(game.status);
+			setMinesCount(game.mines - game.flags);
 		}
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [moveX, moveY, moveType, moveTimestamp]);
 
 	return (
 		<>
 			<AppHeader homeBtn={true}/>
 			<main className="game-container">
-				<StatusBar/>
+				<StatusBar minesCount={minesCount} gameStatus={gameStatus}/>
 				<GameBoard
 					moveTimestamp={setMoveTimestamp}
 					moveX={setMoveX}
@@ -104,7 +91,6 @@ function GamePage() {
 					board={boardData}
 					sizeX={sizeX}
 					sizeY={sizeY}
-					showMines={showMines}
 				/>
 			</main>
 			<div></div>
